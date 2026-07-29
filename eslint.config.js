f155bab6c913e11f0e0a4b94a737915f7fa1efc0
Lakshminarayana Aigal <lakshminarayanaigal@gmail.com>
2026-07-29 11:36:22 +0530

    fix: ignore auto-generated Next.js config files in ESLint and allow CJS in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,14 @@ const tseslint = require("typescript-eslint");
 
 module.exports = tseslint.config(
   {
-    ignores: ["**/dist/**", "**/.next/**", "**/node_modules/**"],
+    ignores: [
+      "**/dist/**",
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/next-env.d.ts",
+      "**/next.config.js",
+      "**/postcss.config.js",
+    ],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
@@ -14,6 +21,13 @@ module.exports = tseslint.config(
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    files: ["eslint.config.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "no-undef": "off",
     },
   },
 );
