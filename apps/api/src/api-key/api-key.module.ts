6a95bce63d6e74749ce0c23bdea0f0239b321756
Lakshminarayana Aigal <lakshminarayanaigal@gmail.com>
2026-08-06 19:42:18 +0530

    refactor(api-key): remove unused ApiKeyGuard

--- apps/api/src/api-key/api-key.module.ts
+++ apps/api/src/api-key/api-key.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { ApiKeyService } from './api-key.service';
 import { ApiKeyController } from './api-key.controller';
-import { ApiKeyGuard } from './api-key.guard';
 
 @Global()
 @Module({
   controllers: [ApiKeyController],
-  providers: [ApiKeyService, ApiKeyGuard],
-  exports: [ApiKeyService, ApiKeyGuard],
+  providers: [ApiKeyService],
+  exports: [ApiKeyService],
 })
 export class ApiKeyModule {}
